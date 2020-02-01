60fd3501ce8f14e81aa9a8f08a228401435ea4b0
Parth Parmar <parthmparmar@gmail.com>
2020-01-31 22:00:05 -0500

    working game with out css

---
+++ clicky-game/src/components/jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Superhero Clicky Game</h1>
+                <p className="lead">How good is you memory?  Good enough to be a Superhero or a Superfan?</p>
+            </div>
+        </div>
+    );
+};
+
+export default Jumbotron;
---
+++ clicky-game/src/images/index.js
@@ -0,0 +1,65 @@
+
+const babyYodaImg = require("./baby_yoda.jpg");
+const batmanImg = require("./batman.jpg");
+const captainAmericaImg = require("./captain_america.jpg");
+const hulkImg = require("./hulk.jpg");
+const ironmanImg = require("./iorn_man.jpg");
+const spidermanImg = require("./spider_man.jpg");
+const stormImg = require("./storm.jpg");
+const subzeroImg = require("./subzero.jpg");
+const supermanImg = require("./superman.jpg");
+const venomImg = require("./venom.jpg");
+const wolverineImg = require("./wolverine.jpg");
+const wonderWomanImg = require("./wonder_woman.jpg");
+
+export const imageArray = [
+    {
+        name: "babyYoda",
+        image: babyYodaImg
+    },
+    {
+        name: "batman",
+        image: batmanImg
+    },
+    {
+        name: "captainAmerica",
+        image: captainAmericaImg
+    },
+    {
+        name: "hulk",
+        image: hulkImg
+    },
+    {
+        name: "ironMan",
+        image: ironmanImg
+    },
+    {
+        name: "spiderman",
+        image: spidermanImg
+    },
+    {
+        name: "storm",
+        image: stormImg
+    },
+    {
+        name: "subzero",
+        image: subzeroImg
+    },
+    {
+        name: "superman",
+        image: supermanImg
+    },
+    {
+        name: "venom",
+        image: venomImg
+    },
+    {
+        name: "wolverine",
+        image: wolverineImg
+    },
+    {
+        name: "wonder_woman",
+        image: wonderWomanImg
+    },
+
+]
