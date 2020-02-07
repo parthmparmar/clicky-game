b461db3903d16c5d5bd187968ffc4e26b6b10ddc
Parth Parmar <parthmparmar@gmail.com>
2020-02-06 22:23:54 -0500

    Updates

---
+++ precache-manifest.cccbd5562524413468dfbe8579379c42.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411961b994f3f4c37ed0985eb523a091",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f7b54e93d1ee3a487224",
+    "url": "/clicky-game/static/css/main.b0398a27.chunk.css"
+  },
+  {
+    "revision": "b712d367c5c12d2dc8c0",
+    "url": "/clicky-game/static/js/2.bf609584.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.bf609584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b54e93d1ee3a487224",
+    "url": "/clicky-game/static/js/main.920acde9.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  },
+  {
+    "revision": "fa1b0be8036e434560954b43c8ab780b",
+    "url": "/clicky-game/static/media/baby_yoda.fa1b0be8.jpg"
+  },
+  {
+    "revision": "640d3f99e05eaae323baed269d2e329d",
+    "url": "/clicky-game/static/media/batman.640d3f99.jpg"
+  },
+  {
+    "revision": "89c4fa2fa0d8e4ef426c002c71284f94",
+    "url": "/clicky-game/static/media/captain_america.89c4fa2f.jpg"
+  },
+  {
+    "revision": "156484dcab6a947d09a8bb3c893aa505",
+    "url": "/clicky-game/static/media/hulk.156484dc.jpg"
+  },
+  {
+    "revision": "6ad21febe985f62dfa47a4dc0b6d1ed3",
+    "url": "/clicky-game/static/media/iorn_man.6ad21feb.jpg"
+  },
+  {
+    "revision": "c6b9716bfce92188dc356993565902bc",
+    "url": "/clicky-game/static/media/spider_man.c6b9716b.jpg"
+  },
+  {
+    "revision": "b9a5a4fd765f9ba5c07cf22c80e0b8b9",
+    "url": "/clicky-game/static/media/storm.b9a5a4fd.jpg"
+  },
+  {
+    "revision": "ee512b8651483a8a5de7af17c4d4d2fb",
+    "url": "/clicky-game/static/media/subzero.ee512b86.jpg"
+  },
+  {
+    "revision": "5a78bb12330349c15150d3bf9eb6ec06",
+    "url": "/clicky-game/static/media/superman.5a78bb12.jpg"
+  },
+  {
+    "revision": "124d942f4ff84917c12ba6a7cd19f975",
+    "url": "/clicky-game/static/media/venom.124d942f.jpg"
+  },
+  {
+    "revision": "0a4af948bac38e6277cfa1348ca746c8",
+    "url": "/clicky-game/static/media/wolverine.0a4af948.jpg"
+  },
+  {
+    "revision": "6cdcd3bd9d2a500c4a61accc38fb1abf",
+    "url": "/clicky-game/static/media/wonder_woman.6cdcd3bd.jpg"
+  }
+]);
